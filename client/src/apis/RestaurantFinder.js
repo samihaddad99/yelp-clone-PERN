3794b8060459410321d426d2865abc930172a59d
Sami Haddad <77640501+samihaddad99@users.noreply.github.com>
2022-09-01 13:31:15 -0400

    initial commit

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -5,10 +5,12 @@ const RestaurantFinder = axios.create({
   baseURL:
     process.env.NODE_ENV !== "production"
       ? "http://localhost:7000/api/v1/restaurants"
-      : "https://alluring-bryce-canyon-75245.herokuapp.com/api/v1/restaurants",
+      : "https://pern-example.herokuapp.com/api/v1/restaurants",
   timeout: 5000,
 });
 
 axiosRetry(RestaurantFinder, { retries: 3 });
 
 export default RestaurantFinder;
+
+
